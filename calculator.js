6fe28d70915a005e2382e403a2d6fe00e543147e
Chantal Chung <chantal.chung@live.com>
2020-02-13 21:54:09 -0700

    Added extra use case handling

--- calculator.js
+++ calculator.js
@@ -1,28 +1,50 @@
 $(document).ready(() => {
 	let cur_entry = 0;
 	let clear_prev = true;
+	let eval_prev = false;
+	let op_prev = false;
+	let num_open_bracket = 0;
+	let num_closed_bracket = 0;
+	let op_count = 0;
 	$('.current').html(cur_entry);
 
 	$('.clear').on('click', function(evt) {
 		let button_pressed = $(this).html();
+		cur_entry = $('.current').html();
+		eval_prev = false;
+		op_prev = false;
 		$('.current').html('0');
-		$('.previous').html('');
+		$('.previous').html('Ans = ' + cur_entry);
 		clear_prev = true;
+		op_count = 0;
 	});
 	$('.back').on('click', function(evt) {
 		let button_pressed = $(this).html();
 		cur_entry = $('.current').html();
-		if (cur_entry === "Err") {
+		eval_prev = false;
+		op_prev = false;
+		if (cur_entry === "Err" || cur_entry.length === 0) {
 			$('.current').html('0')
 			clear_prev = true
 		}
+		else if (cur_entry === '0') {
+			$('.current').html('0')
+		}
 		else{
+			if (['-', '/', '*', '+'].includes(cur_entry[cur_entry.length - 1])) {
+				op_count -= 1;
+			}
 			$('.current').html(cur_entry.slice(0, -1));
 		}
 	});
 	$('.num_button').on('click', function(evt) {
 		let button_pressed = $(this).html();
 		cur_entry = $('.current').html();
+		eval_prev = false;
+		op_prev = false;
+		if (cur_entry.length >= 30) {
+			return;
+		}
 		if (!clear_prev) {
 			$('.current').html(cur_entry + button_pressed);
 		}
@@ -34,27 +56,76 @@ $(document).ready(() => {
 	$('.operator').on('click', function(evt) {
 		let button_pressed = $(this).html();
 		cur_entry = $('.current').html();
-		if (button_pressed === 'x') {
-			button_pressed = '*'
+		if (['-', '/', '+', '*'].includes(cur_entry[cur_entry.length - 1])) {
+			op_prev = true;
 		}
-		if (!clear_prev) {
-			$('.current').html(cur_entry + button_pressed);
+		eval_prev = false;
+		
+		if (cur_entry.length >= 30) {
+			return;
+		}
+		if (button_pressed === 'x') {
+			button_pressed = '*';
+		}
+		if (num_closed_bracket >= num_open_bracket && button_pressed === ')') {
+			return;
+		}
+		if (button_pressed === '(') {
+			num_open_bracket += 1;
+			if (clear_prev) {
+				$('.current').html(button_pressed);
+			}
+			else {
+				$('.current').html(cur_entry + button_pressed);
+			}
+			clear_prev = false;
+			return;
+		}
+		if (op_prev && cur_entry[cur_entry.length - 1] === button_pressed) {
+			clear_prev = false;
+			return;
+		}
+		if (button_pressed === ')') {
+			if (cur_entry[cur_entry.length - 1] !== '(') {
+				$('.current').html(cur_entry + button_pressed);
+				num_closed_bracket += 1;
+			}
+			clear_prev = false;
+			return;
+		}
+		if (op_count >= 2) {
+			return;
+		}
+		else if (op_prev && cur_entry[cur_entry - 1] !== button_pressed && button_pressed !== '-') {
+			$('.current').html(cur_entry.substring(0, cur_entry.length - 1) + button_pressed)
+			op_count -= 1;
 		}
 		else {
-			$('.current').html(button_pressed);
-			clear_prev = false;
+			$('.current').html(cur_entry + button_pressed);
 		}
+		op_prev = true;
+		clear_prev = false;
+		op_count += 1;
 	});
 	$('.eval').on('click', function(evt) {
 		cur_entry = $('.current').html();
+		op_prev = false;
 		try {
 			let result = eval(cur_entry)
 			$('.current').html(result);
-			$('.previous').html(cur_entry);
+			if (eval_prev) {
+				let prev = $('.previous').html();
+				$('previous').html(prev);
+			}
+			else {
+				$('.previous').html(cur_entry + ' = ');
+			}
 		}
 		catch (e) {
 			$('.current').html('Err');
 			$('.previous').html(cur_entry);
 		}
+		eval_prev = true;
+		op_count = 0;
 	});
 })
